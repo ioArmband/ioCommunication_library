0f0fd8999b07daffdc0f6384bd0b7cc13ac5e6c3
VirtualInstict <jean-michel@pothelune.fr>
2014-02-08 20:17:57 +0100

    Add image serialization

--- src/main/java/org/tse/pri/ioarmband/io/message/ImageViewerApp.java
+++ src/main/java/org/tse/pri/ioarmband/io/message/ImageViewerApp.java
@@ -1,21 +1,28 @@
 package org.tse.pri.ioarmband.io.message;
 
 import java.awt.Image;
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.imageio.ImageIO;
 
 public class ImageViewerApp extends Message{
 	private static final long serialVersionUID = 6745634928545223211L;
-	Image image;
-	
+	transient Image image;
+
 	public ImageViewerApp(Image image) {
 		super("image_viewer_app");
 		this.image = image;
 	}
-	
+
 	public Image getImage() {
 		return image;
 	}
 	public void setImage(Image image) {
 		this.image = image;
 	}
-
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		ImageIO.write((RenderedImage) image, "png", out); // png is lossless
+	}
 }
