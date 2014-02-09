8bfb85eb460a4dbd9dacccc028e2ca75266cc2f8
bennetto <bbonnetto@gmail.com>
2014-02-09 17:42:40 +0100

    remove image to TextMessageAppMessage

--- src/main/java/org/tse/pri/ioarmband/io/message/TextMessageAppMessage.java
+++ src/main/java/org/tse/pri/ioarmband/io/message/TextMessageAppMessage.java
@@ -1,16 +1,12 @@
 package org.tse.pri.ioarmband.io.message;
 
 import java.awt.Image;
-import java.awt.image.RenderedImage;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
-import javax.imageio.ImageIO;
 
-public class TextMessageAppMessage extends Message {
+
+public class TextMessageAppMessage extends Message   {
 	private static final long serialVersionUID = 6745634928545223211L;
-	transient Image image;
+
 	String message;
 	String author;
 
@@ -18,10 +14,10 @@ public class TextMessageAppMessage extends Message {
 		super("text_message_app");
 		this.message = message;
 		this.author = author;
-		this.image = image;
 	}
 	public TextMessageAppMessage(String author, String message) {
-		this(author, message, null);
+		super("text_message_app");
+		//this(author, message, null);
 	}
 	
 	public String getAuthor() {
@@ -36,17 +32,6 @@ public class TextMessageAppMessage extends Message {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	public Image getImage() {
-		return image;
-	}
-	public void setImage(Image image) {
-		this.image = image;
-	}
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		ImageIO.write((RenderedImage) image, "jpg", out);
-	}
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        image = ImageIO.read(in);
-    }
+
+
 }
